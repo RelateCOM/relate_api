5ba756a528d3c66c902e9e2fe83eeb243a7e5349
freeeakn <1rycont@gmail.com>
2024-11-22 04:16:41 +0700

    fix cors origins

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // CORS
   app.enableCors({
-    origin: '*',
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
   });
